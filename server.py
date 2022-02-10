7d664929b475cdda020d7de211706bbaf87e10e1
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-10 12:56:03 -0500

    Working

--- server.py
+++ server.py
@@ -1,10 +1,38 @@
-from flask import flask
-app = Flask(__name__)
 
-@app.route('/')
+# Imports
+from flask import Flask  # Import Flask to allow us to create our app
+
+
+# Instatiating from Flask
+app = Flask(__name__) # Create a new instance of the Flask class called "app"
+
+
+
+
+
+
+
+
+
+# Routing Methods
+@app.route('/')             # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/wow')
+def wow():
+    return 'Wooooowwwww!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return "Hello, " + name
 
 
-if __name__=="__main__"
-    app.run(debug=True)
+
+
+
+if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
+
+
